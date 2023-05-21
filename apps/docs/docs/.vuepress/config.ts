f9021458bde02a728683c76b7852e9f0d483202e
Nicole Hollant <nicolehollant@gmail.com>
2023-05-21 15:06:48 -0400

    favicon

--- apps/docs/docs/.vuepress/config.ts
+++ apps/docs/docs/.vuepress/config.ts
@@ -4,6 +4,7 @@ export default defineUserConfig({
   lang: 'en-US',
   title: 'CLUI',
   description: 'little thing so that you can create and save basic UIs around your CLI tools 🥰',
+  head: [['link', { rel: 'icon', href: '/images/favicon.png' }]],
   theme: defaultTheme({
     logo: '/images/favicon.png',
     repo: 'nicolehollant/clui',
